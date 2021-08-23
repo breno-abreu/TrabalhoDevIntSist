275c31328cec76d7d1d697b6bff2dec5f72f0c51
Breno Abreu <57421620+breno-abreu@users.noreply.github.com>
2021-08-23 11:42:46 -0300

    Commit 3

--- client.py
+++ client.py
@@ -5,8 +5,6 @@ import os.path
 import pandas as pd
 import numpy as np
 
-from requests_toolbelt.multipart.encoder import MultipartEncoder
-
 
 URL = "http://localhost:5000"
 
@@ -15,79 +13,122 @@ pw = ""
 token = ""
 
 def main():
-    """opt = 0
+    """ Apresenta um menu de opções para o usuário """
+
+    opt = 0
     print("Bem vindo!\n")
     while opt != "0":
         opt = input("\nDigite o número de uma das opções abaixo!\n" +
-                "Cadastrar - 1\n" +
-                "Login - 2\n" +
-                "Testar token - 3\n" +
-                "Sair - 0\n"
-                ).strip()
+                    "Cadastrar - 1\n" +
+                    "Login - 2\n" +
+                    "Testar token - 3\n" +
+                    "Enviar sinal - 4\n" + 
+                    "Listar imagens - 5\n" +
+                    "Baixar imagem - 6\n" +
+                    "Sair - 0\n"
+                    ).strip()
 
         if opt == "1":
-            username = input("Usuario: ")
-            password = input("Senha: ")
-            data = {'user' : str(username), 'pass' : str(password)}
-            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+            # Cadastra um usuário no servidor
+            register_user()
             
-            resp = requests.post(url = URL+'/cadastrar', data = json.dumps(data), headers=headers)
-            
-            #r = requests.post(url, data=json.dumps(data), headers=headers)
-            if resp.ok:
-                print("Cadastro feito com sucesso!")
-            else:
-                print("O cadastro não foi efetuado!")
-        if opt == "2":
-            username = input("Usuario: ")
-            password = input("Senha: ")
-            data = {'user' : str(username), 'pass' : str(password)}
-            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-            
-            resp = requests.post(url = URL+'/login', data = json.dumps(data), headers=headers)
-            
-            #r = requests.post(url, data=json.dumps(data), headers=headers)
-            if resp.ok:
-                token = (resp.json())['token']
-                print(token)
-                print("Logado com sucesso!")
-            elif resp.status_code == 406:
-                print("Usuario/Senha incorreto!")
-            else:
-                print("Nao foi possivel efetuar o login!")
+        elif opt == "2":
+            # Faz o login de um usuário
+            user_login()
 
-        if opt == "3":  
-            #data = {'token' : str(token)}
-            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-            resp = requests.get(url = URL+'/ping'+'?token='+token, headers=headers) #, data = json.dumps(data), headers=headers)  
-            print(resp.text)
+        elif opt == "3":  
+            # Testa o tolken do usuário
+            test_tolken()
+        
+        elif opt == "4":
+            # Envia um sinal para o servidor
+            send_signal()
+        
+        elif opt == "5":
+            # Recebe uma lista de imagens e suas informações
+            list_images()
 
-        if opt == '0':
-            print("Saindo...")"""
+        elif opt == "6":
+            # Baixa uma imagem do servidor
+            download_image()
+
+        elif opt == '0':
+            print("Saindo...")
+        
+        else:
+            print("Opção não existente!")
     
-    send_signal()
-    #list_images()
-    #download_image()
+
+def register_user():
+    """ Faz o cadastro de um usuário no servidor """
+
+    username = input("Usuario: ")
+    password = input("Senha: ")
+    data = {'user' : str(username), 'pass' : str(password)}
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    resp = requests.post(url = URL+'/cadastrar', data = json.dumps(data), headers=headers)
+    
+    if resp.ok:
+        print("Cadastro feito com sucesso!")
+    else:
+        print("O cadastro não foi efetuado!")
+
+
+def user_login():
+    """ Faz o login de um usuário """
+
+    username = input("Usuario: ")
+    password = input("Senha: ")
+    data = {'user' : str(username), 'pass' : str(password)}
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    
+    resp = requests.post(url = URL+'/login', data = json.dumps(data), headers=headers)
+    
+    if resp.ok:
+        token = (resp.json())['token']
+        print(token)
+        print("Logado com sucesso!")
+    elif resp.status_code == 406:
+        print("Usuario/Senha incorreto!")
+    else:
+        print("Nao foi possivel efetuar o login!")
+
+
+def test_tolken():
+    """ Testa o tolken de um usuário """
+
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    resp = requests.get(url = URL+'/ping'+'?token='+token, headers=headers)
+    print(resp.text)
 
 
 def send_signal():
+    """ Carrega um sinal de um arquivo .csv, faz o cálculo de ganho de sinal
+        e envia-o para o servidor juntamente com outras informações """
+
     resp = None
+
     while True:
         path = input("Informe o caminho do arquivo de sinal: ")
-        path = './files/a-1.csv' #TEMP
+        path = './files/g-1.csv' #TEMP
         file = Path(path)
         extension = os.path.splitext(path)[1]
+
+        # Verifica se o arquivo existe e se a extensão é .csv
         if file.is_file() and extension == '.csv':
             break
         else:
             print("Arquivo não encontrado ou com extensão não suportada!")
     
-    name = input("Informe o nome e a extensão do arquivo de imagem: ")
+    name = input("Informe o nome e a extensão do arquivo de imagem que será salva no servidor: ")
     name = 'g5.png' #TEMP
 
     while True:
         algorithm = input("Escolha o algoritmo utilizado\n[(1) - CGNE | (2) - FISTA]: ")
         algorithm = '1' #TEMP
+
+        # Cria o dicionário que será enviado como json para o servidor contendo
+        # o nome do usuário e o nome do arquivo de imagem que será gerado no servidor
         data = {'user' : un, 'name' : name}
 
         if algorithm != '1' and algorithm != '2':
@@ -100,14 +141,18 @@ def send_signal():
             elif algorithm == '2':
                 data['algorithm'] = 'FISTA'
 
-            vector = pd.read_csv(path, sep=';', header=None)
-            vector = vector.replace(',', '.', regex=True)
-            vector = vector.apply(pd.to_numeric, errors='coerce')
-            vector = vector.to_numpy()
+            # Carrega um arquivo .csv local
+            vector = load_csv_file(path)
 
+            # Realiza o calculo de ganho de sinal
+            vector = calculate_signal_gain(vector)
+
+            # Inseri osinal no dicionário
             data['signal'] = vector.tolist()
-            resp = requests.post(url=URL + '/enviar_sinal', json=data)
-            
+
+            # Envia um post para o servidor contendo o dicionário gerado
+            # e recebe a resposta
+            resp = requests.post(url=URL + '/enviar_sinal' +'?token='+token, json=data)
             break
                
     if resp.ok:
@@ -117,10 +162,21 @@ def send_signal():
 
 
 def list_images():
+    """ Faz o requerimento da lista de arquivos de imagem presentes no servidor
+        de um determinado usuário """
+
+    # Cria um dicionário contendo o parâmetro que será enviado
     payload = {'user' : un}
-    resp = requests.get(url=URL + '/listar_imagens', params=payload)
+
+    # Envia um get para o servidor enviando o nome do usuário como parâmetro
+    # e recebe a resposta
+    resp = requests.get(url=URL + '/listar_imagens' +'?token='+token, params=payload)
+
     if resp.ok:
+        # Recebe um json contendo as informações das imagens
         img_list= resp.json()
+
+        # imprime as informações de todas as imagens
         if len(img_list) > 0:
             for image in img_list:
                 print("Usuário: " + image['username'] + "  Nome: " + image['name'])
@@ -133,19 +189,62 @@ def list_images():
 
 
 def download_image():
+    """ Baixa uma imagem presente no servidor """
+
     name = input("Qual imagem deseja baixar?: ")
-    name = 'g1.png'
+    name = 'g1.png' # TEMP
+
+    # Cria um dicionário contendo os parâmetros da requisição
     payload = {'user' : un, 'name' : name}
-    resp = requests.get(url=URL + '/baixar_imagem', params=payload, stream=True)
+
+    # Envia um get para o servidor juntamente tendo o dicionário como parâmetros
+    # e recebe a resposta
+    resp = requests.get(url=URL + '/baixar_imagem' +'?token='+token, params=payload, stream=True)
     if resp.ok:
-        with open('img.png', 'wb') as file:
+        path = input("Especifique o caminho e o nome do arquivo que será salvo: ")
+        # Salva a imagem recebida na resposta
+        with open(path, 'wb') as file:
             for chunk in resp:
                 file.write(chunk)
-                
+
         print("Imagem salva")
     
     else:
         print("Não foi possível baixar a imagem")
+
+
+def calculate_signal_gain(vector):
+    """ Faz o cálculo de ganho de sinal de um vetor """
+
+    # Transforma o vetor em uma matriz retangular
+    vector = vector.reshape((794, 64))
+
+    # Para cada coluna realiza o algoritmo de cálculo de ganho de sinal
+    for i in range(64):
+        for j in range(794):
+            gamma = 100 + 1 / 20 * j * np.sqrt(j)
+            vector[j][i] *= gamma 
+    
+    # Transforma a matriz em um vetor com as mesmas dimensões do vetor recebido
+    return vector.reshape((50816, 1))
+
+
+def load_csv_file(path):
+    """ Carrega um arquivo .cvs utilziando a bilioteca Pandas e o 
+        transforma em um vetor do NumPy """
+
+    # Carrega o vetor 
+    vector = pd.read_csv(path, sep=';', header=None)
+
+    # Substitui as vírgulas por pontos
+    vector = vector.replace(',', '.', regex=True)
+
+    # Transforma os valores em valores numéricos e caso um deles
+    # tenha um tipo diferente é registrado como NaN
+    vector = vector.apply(pd.to_numeric, errors='coerce')
+
+    # Retorna o vetor transformado em vetor do NumPy
+    return vector.to_numpy()
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,15 +1,6 @@
-#pip install flask-mysqldb
-from operator import truediv
-import time
 import json
 from flask import Flask, request, jsonify, make_response, send_file
-from flask_sqlalchemy import SQLAlchemy
-from flask.wrappers import Response
-from sqlalchemy.orm import defaultload
-from config import app, db, Users, session, Images
-#import psutil
-import sys, os
-import pathlib
+from config import app, db, Users, Images
 from flask import Flask, jsonify, request, make_response
 import pandas as pd
 import numpy as np
@@ -17,201 +8,10 @@ import cv2 as cv
 from numpy import linalg as la
 from threading import Thread
 from datetime import datetime
-
-def run_cgne(data):
-    name = data['name']
-    vector = np.array(data['signal'])
-
-    print(f"Iniciando algoritmo CGNE para o sinal {name}")
-
-    img_path = '/images/' + data['name']
-    start_time = datetime.now()
-
-    path = "./files/H-1.csv"
-    matrix = pd.read_csv(path, header=None)
-    matrix = matrix.apply(pd.to_numeric, errors='coerce')
-    matrix = matrix.to_numpy()
-
-    r = vector
-    p = np.dot(np.transpose(matrix), r)
-    image = np.zeros(len(p))
-    i = 0
-
-    for i in range(40):
-        print(f"[ITERAÇÃO {i}]")
-        # linha 1
-        alpha = np.dot(np.transpose(r), r) / np.dot(np.transpose(p), p)
-
-        # linha 2
-        image = np.add(image, alpha * np.transpose(p))
-
-        # linha 3
-        r_aux = np.copy(r)
-        r = r - (alpha * np.dot(matrix, p))
-
-        # linha 4
-        beta = np.dot(np.transpose(r), r) / np.dot(np.transpose(r_aux), r_aux)
-
-        #linha 5
-        p = np.add(np.dot(np.transpose(matrix), r), beta * p)
-
-        # calculo de erro
-        erro = np.sqrt(np.dot(np.transpose(r), r))
-        if erro < 1e-4:
-            break
-
-    menor = abs(image.min())
-    maior = image.max() + menor
-
-    for i in range(3600):
-        #image[0][i] = (image[0][i] + menor) / maior * 255
-        image[0][i] *= 255
-
-    image = image.reshape((60, 60), order='F')
-    res = cv.resize(image, (600, 600), interpolation = cv.INTER_NEAREST)
-    cv.imwrite('.' + img_path, res)
-
-    finish_time = datetime.now()
-    duration = (finish_time - start_time).total_seconds()
-    duration = str(duration) + 's'
-
-    image = Images(username=data['user'], 
-                   name=data['name'], 
-                   algorithm=data['algorithm'],
-                   path=img_path,
-                   size='600x600',
-                   n_iterations=2,
-                   status='Concluído',
-                   start_time=start_time,
-                   finish_time=finish_time,
-                   duration=duration)
-
-    db.session.add(image)
-    db.session.commit()
-
-    print("Imagem salva")
-    print(Images.query.all())
-
-
-def run_fista(data):
-    name = data['name']
-    vector = np.array(data['signal'])
-
-    print(f"Iniciando algoritmo FISTA para o sinal {name}")
-
-    img_path = '/images/' + data['name']
-    start_time = datetime.now()
-
-    path = "./files/H-1.csv"
-    matrix = pd.read_csv(path, header=None)
-    matrix = matrix.apply(pd.to_numeric, errors='coerce')
-    matrix = matrix.to_numpy()
-
-    image = np.zeros(3600)
-    y = np.zeros(3600)
-    alpha = 1
-
-    for i in range(2):
-        print("[ITERAÇÃO {0}]".format(i + 1))
-        # coeficiente de regularização
-        lam = np.max(np.abs(np.dot(np.transpose(matrix), vector))) * 0.1
-
-        # fator de redução
-        c = la.norm(np.matmul(np.transpose(matrix), matrix), ord=2)
-
-        # linha 1
-        image_old = np.copy(image)
-        x = np.transpose(vector)[0] - np.dot(matrix, y)
-        image = len(vector) * (lam / c) * (np.add(y, np.dot(1 / c * np.transpose(matrix), x)))
-
-        # linha 2
-        alpha_old = alpha
-        alpha = (1 + np.sqrt(1 + 4 * alpha ** 2)) / 2
-
-        # linha 3
-        y = image + ((alpha_old - 1) / alpha) * (image - image_old)
-
-
-    menor = abs(image.min())
-    maior = image.max() + menor
-
-    for i in range(3600):
-        image[i] = (image[i] + menor) / maior * 255
-        #image[i] *= 255
-
-    image = image.reshape((60, 60), order='F')
-    res = cv.resize(image, (600, 600), interpolation = cv.INTER_NEAREST)
-    cv.imwrite('.' + img_path, res)
-
-    finish_time = datetime.now()
-    duration = (finish_time - start_time).total_seconds()
-    duration = str(duration) + 's'
-
-    image = Images(username=data['user'], 
-                   name=data['name'], 
-                   algorithm=data['algorithm'],
-                   path=img_path,
-                   size='600x600',
-                   n_iterations=2,
-                   status='Concluído',
-                   start_time=start_time,
-                   finish_time=finish_time,
-                   duration=duration)
-
-    db.session.add(image)
-    db.session.commit()
-
-    print("Imagem salva")
-    print(Images.query.all())
-    
-
-@app.route('/enviar_sinal', methods=['POST'])
-def get_signal():
-    data = request.json
-
-    if data['algorithm'] == 'FISTA':
-        thread = Thread(target=run_fista, args=(data,))
-        thread.start()
-
-    elif data['algorithm'] == 'CGNE':
-        thread = Thread(target=run_cgne, args=(data,))
-        thread.start()
-    
-    """image = Users(username='breno', password='123')
-
-    db.session.add(image)
-    db.session.commit()"""
-
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
-
-
-@app.route('/listar_imagens', methods=['GET'])
-def get_image_list():
-    username = request.args.get('user')
-    image_list_db = Images.query.filter_by(username=username).all()
-    image_list = []
-    for image in image_list_db:
-        image_list.append(image.asdict())
-
-    return jsonify(image_list)
-
-
-@app.route('/baixar_image', methods=['GET'])
-def download_image():
-    username = request.args.get('user')
-    img_name = request.args.get('name')
-    image = Images.query.filter_by(username=username, name=img_name).first()
-    if image != None:
-        return send_file('.' + image.path, mimetype='image/png')
-    else:
-        print("image não encontrada")
-        return json.dumps({'success':False}), 500, {'ContentType':'application/json'}
-
-
-
-"""import jwt 
+import jwt 
 import datetime
 from functools import wraps
+
 
 def token_required(f):
     @wraps(f)
@@ -230,11 +30,256 @@ def token_required(f):
 
     return decorated
 
+
+def run_cgne(data):
+    """ Aplica o algoritmo CGNE em um sinal recebido """
+
+    try:
+        name = data['name']
+        print(f"Iniciando algoritmo CGNE")
+
+        img_path = '/images/' + name
+        start_time = datetime.now()
+
+        # Carrega a matriz de modelo
+        matrix_path = "./files/H-1.csv"
+        matrix = pd.read_csv(matrix_path, header=None)
+
+        # Transforma os valores em valores numéricos e caso um deles
+        # tenha um tipo diferente é registrado como NaN
+        matrix = matrix.apply(pd.to_numeric, errors='coerce')
+        matrix = matrix.to_numpy()
+
+        # Inicializa as variáveis descritas no algoritmo
+        r = np.array(data['signal'])
+        p = np.dot(np.transpose(matrix), r)
+        image = np.zeros(len(p))
+
+        for i in range(30):
+            print(f"[ITERAÇÃO {i + 1}]")
+            # Linha 1 do algoritmo
+            alpha = np.dot(np.transpose(r), r) / np.dot(np.transpose(p), p)
+
+            # Linha 2 do algoritmo
+            image = np.add(image, alpha * np.transpose(p))
+
+            # Linha 3 do algoritmo
+            r_aux = np.copy(r)
+            r = r - alpha * np.dot(matrix, p)
+
+            # Cálculo de erro. Quando o valor é atingido o algoritmo para.
+            erro = la.norm(r, 2) - la.norm(r_aux, 2)
+            if erro < 1e-4:
+                break
+
+            # Linha 4 do algoritmo
+            beta = np.dot(np.transpose(r), r) / np.dot(np.transpose(r_aux), r_aux)
+
+            #Linha 5 do algoritmo
+            p = np.add(np.matmul(np.transpose(matrix), r), beta * p)
+
+        # Encontra o valor absoluto do menor valor no vetor da imagem
+        min_value = abs(image.min())
+
+        # Encontra o maior valor no vetor da imagem e soma com o menor
+        max_value = image.max() + min_value
+
+        # Percorre o vetor da imagem alterando-o para que os valores finais
+        # fiquem entre 0 e 255, assim podendo salvar a imagem em preto e branco
+        for i in range(3600):
+            image[0][i] = (image[0][i] + min_value) / max_value * 255
+            #image[0][i] *= 255
+
+        # Transforma o vetor em uma matriz de 60x60
+        image = image.reshape((60, 60), order='F')
+
+        # Amplia a imagem em 10 vezes
+        res = cv.resize(image, (600, 600), interpolation = cv.INTER_NEAREST)
+
+        # Salva a imagem
+        cv.imwrite('.' + img_path, res)
+
+        # Calcula a duração total do algoritmo
+        finish_time = datetime.now()
+        duration = (finish_time - start_time).total_seconds()
+        duration = str(duration) + 's'
+
+        # Cria um objeto contendo as informações da imagem
+        image = Images(username=data['user'], 
+                    name=data['name'], 
+                    algorithm=data['algorithm'],
+                    path=img_path,
+                    size='600x600',
+                    n_iterations=2,
+                    status='Concluído',
+                    start_time=start_time,
+                    finish_time=finish_time,
+                    duration=duration)
+
+        # Salva as informações no banco de dados
+        db.session.add(image)
+        db.session.commit()
+
+        print("Imagem salva")
+    
+    except Exception as e:
+        print("Não foi possível gerar a imagem")
+        print(e)
+
+
+def run_fista(data):
+    """ Executa o algoritmo FISTA no vetor recebido """
+    try:
+        name = data['name']
+        vector = np.array(data['signal'])
+
+        print(f"Iniciando algoritmo FISTA")
+
+        img_path = '/images/' + name
+        start_time = datetime.now()
+
+        # Carrega a matriz de modelo
+        path = "./files/H-1.csv"
+        matrix = pd.read_csv(path, header=None)
+
+        # Transforma os valores em valores numéricos e caso um deles
+        # tenha um tipo diferente é registrado como NaN
+        matrix = matrix.apply(pd.to_numeric, errors='coerce')
+        matrix = matrix.to_numpy()
+
+        # Inicializa as variáveis descritas no algoritmo
+        image = np.zeros(3600)
+        y = np.zeros(3600)
+        alpha = 1
+
+        for i in range(2):
+            print("[ITERAÇÃO {0}]".format(i + 1))
+            # Faz o cálculo do coeficiente de regularização
+            lam = np.max(np.abs(np.dot(np.transpose(matrix), vector))) * 0.1
+
+            # Faz o cálculo do fator de redução
+            c = la.norm(np.matmul(np.transpose(matrix), matrix), ord=2)
+
+            # Linha 1 do algoritmo
+            image_old = np.copy(image)
+            x = np.transpose(vector)[0] - np.dot(matrix, y)
+            image = len(vector) * (lam / c) * (np.add(y, np.dot(1 / c * np.transpose(matrix), x)))
+
+            # Linha 2 do algoritmo
+            alpha_old = alpha
+            alpha = (1 + np.sqrt(1 + 4 * alpha ** 2)) / 2
+
+            # Linha 3 do algoritmo
+            y = image + ((alpha_old - 1) / alpha) * (image - image_old)
+
+        # Encontra o valor absoluto do menor valor no vetor da imagem
+        min_value = abs(image.min())
+
+        # Encontra o maior valor no vetor da imagem e soma com o menor
+        max_value = image.max() + min_value
+
+        # Percorre o vetor da imagem alterando-o para que os valores finais
+        # fiquem entre 0 e 255, assim podendo salvar a imagem em preto e branco
+        for i in range(3600):
+            image[i] = (image[i] + min_value) / max_value * 255
+            #image[i] *= 255
+
+        # Transforma o vetor em uma matriz de 60x60
+        image = image.reshape((60, 60), order='F')
+
+        # Amplia a imagem em 10 vezes
+        res = cv.resize(image, (600, 600), interpolation = cv.INTER_NEAREST)
+
+        # Salva a imagem
+        cv.imwrite('.' + img_path, res)
+
+        # Calcula a duração total do algoritmo
+        finish_time = datetime.now()
+        duration = (finish_time - start_time).total_seconds()
+        duration = str(duration) + 's'
+
+        # Cria um objeto contendo as informações da imagem
+        image = Images(username=data['user'], 
+                    name=data['name'], 
+                    algorithm=data['algorithm'],
+                    path=img_path,
+                    size='600x600',
+                    n_iterations=2,
+                    status='Concluído',
+                    start_time=start_time,
+                    finish_time=finish_time,
+                    duration=duration)
+
+        # Salva as informações no banco de dados
+        db.session.add(image)
+        db.session.commit()
+
+        print("Imagem salva")
+    
+    except Exception as e:
+        print("Não foi possível gerar a imagem")
+        print(e)
+    
+
+@app.route('/enviar_sinal', methods=['POST'])
+@token_required
+def get_signal():
+    """ Recebe o sinal enviado pelo cliente e executa um dos algoritmos 
+        criando uma nova thread para a execução do algoritmo escolhido """
+
+    data = request.json
+
+    if data['algorithm'] == 'FISTA':
+        thread = Thread(target=run_fista, args=(data,))
+        thread.start()
+
+    elif data['algorithm'] == 'CGNE':
+        thread = Thread(target=run_cgne, args=(data,))
+        thread.start()
+
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+
+@app.route('/listar_imagens', methods=['GET'])
+@token_required
+def get_image_list():
+    """ Envia a lista de imagens pertencentes ao usuário informado nos parâmetros recebidos """
+
+    username = request.args.get('user')
+
+    # Procura por todas as imagens de um determinado usuário
+    image_list_db = Images.query.filter_by(username=username).all()
+    image_list = []
+    for image in image_list_db:
+        image_list.append(image.asdict())
+
+    # Envia a lista contendo as informações das imagens em formato json
+    return jsonify(image_list)
+
+
+@app.route('/baixar_imagem', methods=['GET'])
+@token_required
+def download_image():
+    """ Envia a imagem requisitada pelo usuário """
+
+    username = request.args.get('user')
+    img_name = request.args.get('name')
+
+    # Procura no banco de dados o registro da imagem requerida 
+    image = Images.query.filter_by(username=username, name=img_name).first()
+    if image != None:
+        # Envia a imagem para o cliente
+        return send_file('.' + image.path, mimetype='image/png')
+    else:
+        print("Imagem não encontrada")
+        return json.dumps({'success':False}), 500, {'ContentType':'application/json'}
+
+
 def get_account(user):
     return Users.query.filter_by(username=user).first()
 
-def verifica_login(data):
 
+def verifica_login(data):
     account = get_account(data['user'])
     print(data['pass'])
     print(account.password)
@@ -242,10 +287,12 @@ def verifica_login(data):
         return account
     return None
 
-@app.route('/ping', methods=['get'])
+
+@app.route('/ping', methods=['GET'])
 @token_required
 def ping():
     return jsonify({'message' : 'Seu token é valido!.'})
+
 
 @app.route('/cadastrar', methods=['POST'])
 def cadastrar():
@@ -259,26 +306,26 @@ def cadastrar():
             db.session.commit()
             return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
-    
     return json.dumps({'success':False}), 406, {'ContentType':'application/json'}
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    
     account = verifica_login(request.json)
     if account:
         token = jwt.encode({'user' : account.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(seconds=50)}, app.config['SECRET_KEY'],  algorithm="HS256")
         return jsonify({'token' : token}) #.decode('UTF-8')
 
-    return make_response('Could not verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})"""
+    return make_response('Could not verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
+
+
+if __name__ == "__main__":
+    """ Executa o servidor """
+    app.run(debug=True,threaded=True)
+
 
 # TODO : medir cpu e memoria enquanto os algortmos rodam
 # gives a single float value
 #psutil.cpu_percent()
 # gives an object with many fields
 #psutil.virtual_memory()
-
-if __name__ == "__main__":
-    """ Executa o servidor """
-    app.run(debug=True,threaded=True)
